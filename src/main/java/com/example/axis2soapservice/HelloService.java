ee388f34b4384310d1446c2d7cc9aa602b991a4d
chamseddinenacer <chamseddine.nacer@isimg.tn>
2024-10-03 16:21:15 +0100

    service

---
+++ src/main/java/com/example/axis2soapservice/HelloService.java
@@ -0,0 +1,7 @@
+package com.example.axis2soapservice;
+
+public class HelloService {
+    public int addTwoNumbers(int firstNumber , int secondNumber) {
+        return firstNumber + secondNumber;
+    }
+}
